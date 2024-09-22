0a54c73afeed4a4794dd748a1c6319c1adc7eb2f
Asitha Mudiyanselage <asitha_mudiyanselage@baylor.edu>
2024-09-22 15:54:24 -0500

    add few lines to question 5

--- 5_datastore.py
+++ 5_datastore.py
@@ -45,3 +45,12 @@ datastore = { "medical":[
       ]
 }
 
+outfile =open('retail_space.csv','w')
+
+outfile.write('room-number','use','sq-ft','price\n')
+
+for line in datastore['medical']:
+    
+    outfile.write(f"{line['room-number']},{line['use']},{line['sq-ft']},{line['price']}\n")
+
+outfile.close()
