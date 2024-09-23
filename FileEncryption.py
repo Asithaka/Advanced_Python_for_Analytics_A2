14d7392da195af5c4b7679148b9fc1f7d34d9c04
Asitha Mudiyanselage <asitha_mudiyanselage@baylor.edu>
2024-09-23 01:05:33 -0500

    Add different symbols into  dictionary and add encoding='utf-8'

--- FileEncryption.py
+++ FileEncryption.py
@@ -6,14 +6,14 @@
 #          "u": "'['", "U" : "'21'", "v" : "<", "V" : "'22'","w": "|", "W" : "'23'", "x" : "=", "X" : "'24'","y" : ",", "Y" : "'25'",
 #         "z": "']'", "Z" : "'26'"} 
 
-codes = { "a": "%", "A" : "1"   , "b" : "@",   "B" : "2"   ,"c": "&", "C" : "3"   , "d" : "#", "D" : "4"   ,"e" : "*", "E" : "5",
-          "f": "$", "F" : "6"   , "g" : "(", "G" : "7"   ,"h": ">", "H" : "8"   , "i" : ":", "I" : "9"   ,"j" : "-", "J" : "10",
-          "k": "{", "K" : "'11'", "l" : ")", "L" : "'12'","m": "/", "M" : "'13'", "n" : "!", "N" : "'14'","o" : "_", "O" : "'15'",
-          "p": "]", "P" : "'16'", "q" : "?",   "Q" : "'17'","r": "+", "R" : "'18'", "s" : "`", "S" : "'19'","t" : "~", "T" : "'20'",
-          "u": "[", "U" : "'21'", "v" : "<",   "V" : "'22'","w": "|", "W" : "'23'", "x" : "=", "X" : "'24'","y" : ",", "Y" : "'25'",
-          "z": "}", "Z" : "'26'"} 
+codes = { "a": "`", "A" : "1", "b" : "~", "B" : "2",   "c": "!", "C" : "3",    "d" : "@", "D" : "4"   ,"e" : "#", "E" : "5",
+          "f": "$", "F" : "6", "g" : "%", "G" : "7",   "h": "^", "H" : "8",    "i" : "&", "I" : "9"   ,"j" : "*", "J" : "0",
+          "k": "(", "K" : ";", "l" : ")", "L" : "'","m": "-", "M" : "_", "n" : "+", "N" : "=","o" : "{", "O" : "}",
+          "p": "[", "P" : "]", "q" : "|", "Q" : " \ ", "r": ":", "R" : "ψ", "s" : "σ", "S" : "<","t" : ",", "T" : ">",
+          "u": ".", "U" : "?", "v" : "/", "V" : "¥","w": "€", "W" : "¢", "x" : "£", "X" : "♦","y" : "§", "Y" : "©",
+          "z": "™", "Z" : "Ω", '.' : "Σ", ":" : "μ"} 
 
-infile =open('info_security-1.txt','r')
+infile =open('info_security-1.txt','r', encoding='utf-8')
 
 file_content = infile.read()
 
@@ -34,13 +34,12 @@ for word in my_list:
 
             encrypted_word.append(encrypted_letter)
            
-
     encrypted_sentence.append(encrypted_word)
 
 
-#print(encrypted_sentence)
+print(encrypted_sentence)
 
-outfile = open('encrypted.txt','w')
+outfile = open('encrypted.txt','w', encoding='utf-8')
 
 for item in encrypted_sentence:
 
@@ -48,9 +47,9 @@ for item in encrypted_sentence:
 
         outfile.write(f"{item[j]}")
        
-    outfile.write("\t")
+    outfile.write(" ")
 
-outfile.write(".") 
+#outfile.write(".") 
 
 outfile.close()
 
